f514dd2839eaf0f49042ed1441e423600f1b104e
koop212 <46766458+koop212@users.noreply.github.com>
2019-04-23 11:49:22 -0500

    finished the base mode

---
+++ database.sql
@@ -0,0 +1,42 @@
+How do you get all users from Chicago?
+SELECT * FROM "accounts"
+WHERE "city" ILIKE 'Chicago';
+
+How do you get all users with usernames that contain the letter a?
+SELECT * FROM "accounts"
+WHERE "username" ILIKE '%a%';
+
+The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
+UPDATE "accounts"
+SET "account_balance" = 10
+WHERE ("transactions_completed" = 0 AND "transactions_attempted" = 0);
+
+How do you select all users that have attempted 9 or more transactions?
+SELECT * FROM "accounts"
+WHERE "transactions_attempted" >= 9;
+
+How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+SELECT * FROM "accounts"
+WHERE "account_balance" > 0
+ORDER BY "account_balance" DESC
+LIMIT 3;
+
+How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+SELECT * FROM "accounts"
+WHERE "account_balance" > 0
+ORDER BY "account_balance" ASC
+LIMIT 3;
+
+How do you get all users with account balances that are more than $100?
+SELECT * FROM "accounts"
+WHERE "account_balance" > 100;
+
+How do you add a new account?
+INSERT INTO "accounts" ("username", "city", "transactions_completed", "transactions_attempted", "account_balance")
+VALUES ('bobby', 'minneapolis', 0, 0, 675000000.01);
+
+The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+SELECT * FROM "accounts"
+WHERE ("city" IN ('phoenix', 'miami') AND "transactions_completed" < 5);
+DELETE FROM "accounts"
+WHERE ("city" IN ('phoenix', 'miami') AND "transactions_completed" < 5);
